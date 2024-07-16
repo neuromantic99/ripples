ff6b82b80d9525aaa17bbc9c6a80af6b4ca16136
neuromantic99 <jrowlandm@gmail.com>
2024-07-16 15:33:21 +0100

    restructure code, move some functions aorund

--- ripples/ripple_detection.py
+++ ripples/ripple_detection.py
@@ -1,7 +1,8 @@
 from typing import List
 import numpy as np
 
-from ripples.utils import CandidateEvent, bandpass_filter, compute_envelope
+from ripples.models import CandidateEvent, RotaryEncoder
+from ripples.utils import bandpass_filter, compute_envelope
 
 
 def length_check(candidate_events: List[CandidateEvent]) -> List[CandidateEvent]:
@@ -10,7 +11,7 @@ def length_check(candidate_events: List[CandidateEvent]) -> List[CandidateEvent]
 
 def event_power_check(
     candidate_events: List[CandidateEvent], comparison_power: np.ndarray
-):
+) -> List[CandidateEvent]:
     return [
         event
         for event in candidate_events
@@ -88,3 +89,72 @@ def remove_duplicate_ripples(
             filtered_ripples.append(ripples[i])
 
     return filtered_ripples
+
+
+def get_resting_ripples(
+    ripples: List[CandidateEvent], rotary_encoder: RotaryEncoder, threshold: float
+) -> List[CandidateEvent]:
+    """Double check this is 100% correct. I also don't love the interpolation when it only samples during actual movement"""
+    resting_ripples = []
+    for ripple in ripples:
+        ripple_speed = np.interp(
+            ripple.peak_time, rotary_encoder.time, rotary_encoder.speed
+        )
+        if abs(ripple_speed) < threshold:
+            resting_ripples.append(ripple)
+
+    return resting_ripples
+
+
+def detect_ripple_events(
+    channel: np.ndarray | List[int | float],
+) -> List[CandidateEvent]:
+    median = np.median(channel)
+    upper_threshold = median * 5
+    lower_threshold = median * 2.5
+    candidate_events: List[CandidateEvent] = []
+
+    in_event = False
+    upper_exceeded = False
+    start_event = 0
+    peak_power = -np.inf
+
+    for idx, value in enumerate(channel):
+
+        if value > lower_threshold and not in_event:
+            start_event = idx
+            in_event = True
+
+        if in_event and value > peak_power:
+            peak_power = value
+            peak_idx = idx
+
+        # If you bounce on the lower threshold
+        if value < lower_threshold and in_event and not upper_exceeded:
+            in_event = False
+
+        if value > upper_threshold:
+            upper_exceeded = True
+
+        if value < lower_threshold and in_event and upper_exceeded:
+            in_event = False
+            upper_exceeded = False
+            candidate_events.append(
+                CandidateEvent(
+                    onset=start_event,
+                    offset=idx,
+                    peak_power=peak_power,
+                    peak_idx=peak_idx,
+                )
+            )
+            peak_power = -np.inf
+
+    return candidate_events
+
+
+def get_candidate_ripples(
+    lfp: np.ndarray, sampling_rate: int
+) -> List[List[CandidateEvent]]:
+    """Gets candidate ripples from a common average referenced LFP"""
+    ripple_band = compute_envelope(bandpass_filter(lfp, 125, 250, sampling_rate))
+    return [detect_ripple_events(channel) for channel in ripple_band]
--- ripples/utils.py
+++ ripples/utils.py
@@ -1,63 +1,7 @@
-from typing import List
+from typing import List, TypeVar
 
 import numpy as np
 from scipy import signal
-
-from ripples.models import CandidateEvent
-
-
-def get_candidate_ripples(
-    lfp: np.ndarray, sampling_rate: int
-) -> List[List[CandidateEvent]]:
-    """Gets candidate ripples from a common average referenced LFP"""
-    ripple_band = compute_envelope(bandpass_filter(lfp, 125, 250, sampling_rate))
-    return [detect_ripple_events(channel) for channel in ripple_band]
-
-
-def detect_ripple_events(
-    channel: np.ndarray | List[int | float],
-) -> List[CandidateEvent]:
-    median = np.median(channel)
-    upper_threshold = median * 5
-    lower_threshold = median * 2.5
-    candidate_events: List[CandidateEvent] = []
-
-    in_event = False
-    upper_exceeded = False
-    start_event = 0
-    peak_power = -np.inf
-
-    for idx, value in enumerate(channel):
-
-        if value > lower_threshold and not in_event:
-            start_event = idx
-            in_event = True
-
-        if in_event and value > peak_power:
-            peak_power = value
-            peak_idx = idx
-
-        # If you bounce on the lower threshold
-        if value < lower_threshold and in_event and not upper_exceeded:
-            in_event = False
-
-        if value > upper_threshold:
-            upper_exceeded = True
-
-        if value < lower_threshold and in_event and upper_exceeded:
-            in_event = False
-            upper_exceeded = False
-            candidate_events.append(
-                CandidateEvent(
-                    onset=start_event,
-                    offset=idx,
-                    peak_power=peak_power,
-                    peak_idx=peak_idx,
-                )
-            )
-            peak_power = -np.inf
-
-    return candidate_events
 
 
 def bandpass_filter(
@@ -78,3 +22,15 @@ def shuffle(x: np.ndarray) -> np.ndarray:
     x = np.ravel(x)
     np.random.shuffle(x)
     return x.reshape(shape)
+
+
+def flatten(xss: List[List]) -> List:
+    return [x for xs in xss for x in xs]
+
+
+T = TypeVar("T", float, np.ndarray)
+
+
+def degrees_to_cm(degrees: T) -> T:
+    WHEEL_CIRUMFERENCE = 48  # Have not actually measured the ephys rig.
+    return (degrees / 360) * WHEEL_CIRUMFERENCE
